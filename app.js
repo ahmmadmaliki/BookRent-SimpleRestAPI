b26263b108827e70a1287290315a56a9242cb8b9
ahmmadmaliki <ahmmad.maliki96@gmail.com>
2019-08-16 21:50:09 +0700

    changes 1

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const app = express()
 const logger = require('morgan')   
 const bodyParser = require('body-parser')
 const UserRoute = require('./src/routes/api')
-
-const jwt=require('jsonwebtoken')
 const port = process.env.SERVER_PORT || 3030
 
 
@@ -19,47 +17,3 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/bookrent', UserRoute)
 
-app.post('/bookrent/login', (req, res)=>{
-    const user = {
-        id: 1,
-        username: 'maliki',
-        email: 'amadmaliki@gmail.com'
-    }
-    jwt.sign(user,'secretkey',{expiresIn: '30s'}, (err,token)=>{
-        res.json({
-            token
-        })
-    })
-})
-app.post('/bookrent/post',verifyToken,(req,res)=>{
-    jwt.verify(req.token, 'secretkey', (err,authData)=>{
-        if(err){
-            res.send(403)
-        }else{
-            res.json({
-                message: 'Post created...',
-                authData
-            })
-        }
-    
-    })
-})
-// FORMAT OF TOKEN
-// Authorization: Bearer <access_token>
-function verifyToken(req, res, next){
-    //Get auth header value
-    const bearerHeader = req.headers.authorization
-    if(typeof bearerHeader !== 'undefined'){
-        //Split at the space
-        const bearer= bearerHeader.split(' ')
-        //get token from array
-        const bearerToken= bearer[1]
-        //set the token
-        req.token= bearerToken
-         //next middleware
-            next()
-    } else{
-        res.sendStatus(403)
-    }
-   
-}
